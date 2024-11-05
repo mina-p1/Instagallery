9aedecfb7620f769c521aeb26825f36d05798251
mina-p1 <minap@sheridancollege.ca>
2024-11-04 20:05:04 -0500

    1st iteration done - peter mina (photo controller & model, index and lightbox html)

--- Instagallery/Controllers/PhotoController.cs
+++ Instagallery/Controllers/PhotoController.cs
@@ -1,12 +1,38 @@
-using System;
+// By Peter Mina
+
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Instagallery.Controllers
 {
-    public class PhotoController
+    public class PhotoController : Controller
     {
-        
+        private readonly IPhotoService _photoService;
+
+        public PhotoViewController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
+
+        // Display the main gallery with grid layout
+        [HttpGet]
+        public async Task<IActionResult> Gallery()
+        {
+            var photos = await _photoService.GetAllPhotosAsync(); // API Integration coming later
+            return View(photos);
+        }
+
+        // Display a single photo in lightbox view
+        [HttpGet]
+        public async Task<IActionResult> Lightbox(int id)
+        {
+            var photo = await _photoService.GetPhotoByIdAsync(id); // API Integration coming later
+            if (photo == null)
+            {
+                return NotFound();
+            }
+            return View(photo);
+        }
     }
 }
--- Instagallery/Models/PhotoModel.cs
+++ Instagallery/Models/PhotoModel.cs
@@ -1,12 +1,17 @@
+// By Peter Mina
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Instagallery.Models
 {
     public class PhotoModel
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; } // Path to the photo file
+        public DateTime UploadDate { get; set; }
+        public string Description { get; set; }
+        public string Tags { get; set; } // Comma-separated tags for search
+        public bool IsPublic { get; set; } // Privacy setting
     }
 }
